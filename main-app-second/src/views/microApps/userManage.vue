7e8e18d662ef826e32fd9d41fd7bf5b5405ec2ef
卢方舟 <735417031@qq.com>
2022-07-08 09:53:44 +0800

    æ增加用户管理

---
+++ main-app-second/src/views/microApps/userManage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bdcp">
+    <!--      baseroute="/child-app-1"-->
+    <micro-app
+      name="bdcp"
+      url="http://localhost:5001/bdcp/"
+      inline
+      disableSandbox
+      @mounted="handleMount"
+      @unmount="handleUnmount"
+    ></micro-app>
+  </div>
+</template>
+
+<script setup>
+const handleMount = () => {
+  console.log('子应用-bdcp渲染了')
+}
+const handleUnmount = () => {
+  console.log('子应用-bdcp卸载了')
+}
+</script>
+
+<style
+  scoped
+  lang="scss"
+>
+.test {
+}
+</style>
